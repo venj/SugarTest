a69ca37aa9560ae513e7eeffdaf96aa898d3512a
Venj Chu <ersaclarke@gmail.com>
2015-12-09 13:12:48 +0800

    dependency is actually not required, but why is import required???

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SugarTest",
     dependencies: [
-        .Package(url: "https://github.com/venj/COpenSSL.git", versions: Version(0,0,1) ..< Version(1,0,0)),
+        //.Package(url: "https://github.com/venj/COpenSSL.git", versions: Version(0,0,1) ..< Version(1,0,0)),
         .Package(url: "https://github.com/venj/Sugar.git", versions: Version(0,0,1) ..< Version(1,0,0)),
     ]
 )
