d45eb4517e4ef662eef1d568bdcd72fc36d36316
Venj Chu <ersaclarke@gmail.com>
2015-12-10 21:19:48 +0800

    cheat

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SugarTest",
     dependencies: [
-        //.Package(url: "https://github.com/venj/COpenSSL.git", versions: Version(0,0,1) ..< Version(1,0,0)),
         .Package(url: "https://github.com/venj/Sugar.git", versions: Version(0,0,1) ..< Version(1,0,0)),
     ]
 )
